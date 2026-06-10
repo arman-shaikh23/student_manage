687e3b0954b8ae150a81ac75e5cd922cccfbe03d
Arman2337 <23it119@charusat.edu.in>
2026-06-10 16:31:59 +0530

    Remove root server.js workaround

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./backend/server.js');
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} (started from root workaround)`);
-});
