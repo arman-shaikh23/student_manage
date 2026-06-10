609c7ffed539f200234f7ca3e85ee528b294a80c
Arman2337 <23it119@charusat.edu.in>
2026-06-10 17:22:28 +0530

    Configure backend for Vercel deployment

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -10,7 +10,7 @@ if (!fs.existsSync(uploadDir)) {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, '/tmp');
   },
   filename: function (req, file, cb) {
     cb(null, `student-${Date.now()}${path.extname(file.originalname)}`);
