be7020e0842b377b53c6587a3b96013085db0b51
Arman2337 <23it119@charusat.edu.in>
2026-06-11 16:16:14 +0530

    update helmet CSP to allow swagger CDNs

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,17 @@ app.use((req, res, next) => {
   console.log(`[REQUEST] ${req.method} ${req.url}`);
   next();
 });
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com"],
+      styleSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com", "https://fonts.googleapis.com"],
+      imgSrc: ["'self'", "data:", "https://validator.swagger.io"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
+    },
+  },
+}));
 app.use(cors({
   origin: function (origin, callback) {
     const allowedOrigins = [
